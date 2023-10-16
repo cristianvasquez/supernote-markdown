2063f97bd367726822c68fbfae6dad3554ae51a6
Cristian <cristianvasquez@gmail.com>
2023-10-16 15:58:05 +0200

    add sync

--- sync-notes.sh
+++ sync-notes.sh
@@ -2,11 +2,10 @@
 
 # Get the directory where this script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DOWNLOAD_DIR="$SCRIPT_DIR/supernote"
 
 # Activate the virtual environment
 source "$SCRIPT_DIR/venv/bin/activate"
-
-DOWNLOAD_DIR="$SCRIPT_DIR/supernote"
 
 # Run the Python script
 python "$SCRIPT_DIR/main.py" "$DOWNLOAD_DIR"
@@ -19,7 +18,9 @@ deactivate
 OBSIDIAN_DIR="/home/cvasquez/obsidian/workspace/supernote"
 
 # Execute rsync with mirror sync options
-# rsync -av --delete "$DOWNLOAD_DIR/" "$OBSIDIAN_DIR/"
+
+echo "Sync $DOWNLOAD_DIR into $OBSIDIAN_DIR"
+rsync -av --delete "$DOWNLOAD_DIR/" "$OBSIDIAN_DIR/"
 
 echo "Mirror sync completed"
 
